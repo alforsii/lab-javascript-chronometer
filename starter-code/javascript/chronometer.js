d852a884f67ee90a513673e0a425491a4f8fc2d8
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2019-11-19 01:15:29 -0500

    working on callback and async functions

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,44 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId;
+  }
+  startClick() {
+    // if (this.currentTime) throw new Error("Stopwatch has already started.");
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      // console.log(this.currentTime); //test-it's counting.
+    }, 1000);
+  }
+  getMinutes() {
+    // if (!this.currentTime) return 0;
+    // let minutes = 0;
+    // this.intervalId = setInterval(() => {
+    //   if (this.currentTime === 60) {
+    //     minutes++;
+    //   }
+    //   //    console.log(this.currentTime);
+    //   // console.log(minutes);
+    // }, 1000);
+    // return Number(minutes);
+    let minutes = Math.floor(this.currentTime / 60);
+    // console.log(minutes);
+    return minutes;
+  }
+  getSeconds() {
+    if (!this.currentTime) return 0;
+    let sec = Math.floor(this.currentTime);
+    if (sec > 60) sec = 0;
+    return sec;
+  }
+  twoDigitsNumber() {}
+  stopClick() {
+    // if (!this.currentTime) throw new Error("Stopwatch is not started.");
+
+    clearInterval(this.intervalId);
+  }
+  resetClick() {
+    this.currentTime = 0;
+  }
+  splitClick() {}
 }
