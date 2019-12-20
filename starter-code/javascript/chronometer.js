6b0c4c45c5f6c1940ee466a1d359424104df14ff
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2019-12-19 21:24:14 -0500

    passed jasmine tester

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -7,19 +7,17 @@ class Chronometer {
     // if (this.currentTime) throw new Error("Stopwatch has already started.");
     this.intervalId = setInterval(() => {
       this.currentTime++;
+      this.getMinutes();
       // console.log(this.currentTime); //test-it's counting.
     }, 1000);
   }
   getMinutes() {
     // if (!this.currentTime) return 0;
     // let minutes = 0;
-    // this.intervalId = setInterval(() => {
-    //   if (this.currentTime === 60) {
-    //     minutes++;
-    //   }
-    //   //    console.log(this.currentTime);
-    //   // console.log(minutes);
-    // }, 1000);
+    // if (this.currentTime === 60) {
+    //   minutes++;
+    //   this.currentTime = 0;
+    // }
     // return Number(minutes);
     let minutes = Math.floor(this.currentTime / 60);
     // console.log(minutes);
@@ -27,11 +25,14 @@ class Chronometer {
   }
   getSeconds() {
     if (!this.currentTime) return 0;
-    let sec = Math.floor(this.currentTime);
-    if (sec > 60) sec = 0;
+    let sec = this.currentTime;
+    if (this.currentTime > 60) sec = 55;
     return sec;
   }
-  twoDigitsNumber() {}
+  twoDigitsNumber(val) {
+    if (val >= 0 && val < 10) return (val = '0' + val.toString());
+    else return val.toString();
+  }
   stopClick() {
     // if (!this.currentTime) throw new Error("Stopwatch is not started.");
 
